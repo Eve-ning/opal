13012d67c42d84d39924378fbc0e685515d58924
Evening <johnchangqi@hotmail.com>
2022-07-15 21:25:38 +0800

    Update model test for new schema

--- tests/replay_tests/conftest.py
+++ tests/replay_tests/conftest.py
@@ -8,7 +8,7 @@ THIS_DIR = Path(__file__).parent
 
 
 def pickled_data(pkl_path: Path) -> Tuple[pd.DataFrame, pd.DataFrame]:
-    df = pd.read_pickle(pkl_path).drop('offset', axis=1)
+    df = pd.read_pickle(pkl_path).fillna(0)
     return df.drop('error', axis=1), df['error']
 
 
