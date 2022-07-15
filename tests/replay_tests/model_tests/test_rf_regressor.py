5b96137651e13e4c742da0f4d3023a4f97bfc439
Evening <johnchangqi@hotmail.com>
2022-07-15 10:31:18 +0800

    Update test_rf_regressor.py

--- tests/replay_tests/model_tests/test_rf_regressor.py
+++ tests/replay_tests/model_tests/test_rf_regressor.py
@@ -1,9 +1,14 @@
+import pytest
 from junit_xml import TestSuite, TestCase
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import KFold
 
 
-def test_rf_regressor(rep_data):
+@pytest.mark.parametrize(
+    'n_estimators',
+    (5, 15, 35)
+)
+def test_rf_regressor(rep_data, n_estimators):
     X, y = rep_data
     skf = KFold(n_splits=3, shuffle=True, random_state=0)
 
@@ -12,12 +17,14 @@ def test_rf_regressor(rep_data):
     for e, (train_index, test_index) in enumerate(skf.split(X, y)):
         X_train, X_test = X.iloc[train_index], X.iloc[test_index]
         y_train, y_test = y.iloc[train_index], y.iloc[test_index]
-        rfg = RandomForestRegressor(verbose=1, n_estimators=3)
+        rfg = RandomForestRegressor(verbose=1, n_estimators=n_estimators)
         rfg.fit(X_train, y_train)
         test_cases.append(TestCase(
-            f'Test {e}', f'Score: {rfg.score(X_test, y_test)}'
+            f'RF Regressor Model ({e}):',
+            classname=f"{n_estimators} Estimators",
+            stdout=f'Score: {rfg.score(X_test, y_test)}'
         ))
 
-    ts = TestSuite("RF Regressor suite", test_cases)
+    ts = TestSuite("RF Regressor Suite", test_cases)
     with open('score.xml', 'w') as f:
         TestSuite.to_file(f, [ts])
