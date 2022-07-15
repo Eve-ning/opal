564d0cb1c952963c3bdfa8ef1d12630a785a0424
Evening <johnchangqi@hotmail.com>
2022-07-15 10:44:41 +0800

    Fix issue with overwriting file

--- tests/replay_tests/model_tests/test_rf_regressor.py
+++ tests/replay_tests/model_tests/test_rf_regressor.py
@@ -3,16 +3,21 @@ from junit_xml import TestSuite, TestCase
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import KFold
 
+@pytest.fixture()
+def test_cases():
+    tests = []
+    yield tests
+    ts = TestSuite("RF Regressor Suite", test_cases)
+    with open('rf_regressor_score.xml', 'a+') as f:
+        TestSuite.to_file(f, [ts])
 
 @pytest.mark.parametrize(
     'n_estimators',
     (5, 15, 35)
 )
-def test_rf_regressor(rep_data, n_estimators):
+def test_rf_regressor(rep_data, n_estimators, test_cases):
     X, y = rep_data
     skf = KFold(n_splits=3, shuffle=True, random_state=0)
-
-    test_cases = []
 
     for e, (train_index, test_index) in enumerate(skf.split(X, y)):
         X_train, X_test = X.iloc[train_index], X.iloc[test_index]
@@ -23,7 +28,3 @@ def test_rf_regressor(rep_data, n_estimators):
             f'{n_estimators} Estimators ({e}): {rfg.score(X_test, y_test)}',
             classname=f"RF Regressor Model",
         ))
-
-    ts = TestSuite("RF Regressor Suite", test_cases)
-    with open('score.xml', 'a+') as f:
-        TestSuite.to_file(f, [ts])
