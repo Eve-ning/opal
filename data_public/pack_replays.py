8e1959184d340ee7b2ec1d433ce21df03f0527ab
Evening <johnchangqi@hotmail.com>
2022-07-15 10:12:30 +0800

    Add data for testing

---
+++ data_public/pack_replays.py
@@ -0,0 +1,7 @@
+import os.path
+import tarfile
+
+
+def make_tarfile(output_filename, source_dir):
+    with tarfile.open(output_filename, "w:gz") as tar:
+        tar.add(source_dir, arcname=os.path.basename(source_dir))
