7454e85ebfd83cb260c9138482cb9222bd6ab1de
Evening <johnchangqi@hotmail.com>
2022-12-04 20:08:33 +0800

    Add datadir into conf

--- opal/conf/conf.py
+++ opal/conf/conf.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
-OSU_DIR = Path(__file__).parents[2] / "data/osu/"
+DATA_DIR = Path(__file__).parents[2] / "data/"
+OSU_DIR = DATA_DIR / "osu/"
 REPLAYS_DIR = OSU_DIR / "replays"
 SCORES_DIR = OSU_DIR / "scores"
