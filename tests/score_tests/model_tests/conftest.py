1e876842cd83277b6caee13e3382d232326e092a
Evening <johnchangqi@hotmail.com>
2022-12-13 18:36:16 +0800

    Update sys append path

--- tests/score_tests/model_tests/conftest.py
+++ tests/score_tests/model_tests/conftest.py
@@ -7,4 +7,4 @@ from opal.conf.conf import ROOT_DIR
 
 @pytest.fixture(scope='session', autouse=True)
 def add_submod_path():
-    sys.path.append((ROOT_DIR / "data_ppy_sh_to_csv").as_posix())
+    sys.path.append((ROOT_DIR / "opal/data_ppy_sh_to_csv").as_posix())
