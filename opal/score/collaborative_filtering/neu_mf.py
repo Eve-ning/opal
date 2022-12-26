1c90f19a89560b3d81e70d8f7c70be07fb7f8a5f
Eve-ning <johnchangqi@hotmail.com>
2022-12-26 10:52:01 +0800

    Add separate step function to wrap boilerplate

--- opal/score/collaborative_filtering/neu_mf.py
+++ opal/score/collaborative_filtering/neu_mf.py
@@ -25,32 +25,36 @@ class NeuMF(pl.LightningModule):
         return self.scaler.inverse_transform(val.detach().numpy())
 
     def training_step(self, batch, batch_idx):
-        x_uid, x_mid, y_true = batch
-        y_pred = self(x_uid, x_mid)
+        x_uid, x_mid, y_pred, y_true, y_pred_real, y_true_real = self.step(batch)
 
         # if batch_idx % 32 == 0:
         #     self.logger.experiment.add_histogram("pred", y_pred)
         #     self.logger.experiment.add_histogram("true", y_true)
 
         loss = self.loss(y_pred, y_true)
-        self.log(
-            "train_mae",
-            np.abs(self.scaler_inverse(y_pred) - self.scaler_inverse(y_true)).mean()
-        )
+        self.log("Train MAE (%)", np.abs(y_pred_real - y_true_real).mean(), prog_bar=True)
 
         return loss
 
     def validation_step(self, batch, batch_idx):
-        x_uid, x_mid, y_true = batch
-        y_pred = self(x_uid, x_mid)
-        self.log(
-            "val_mae",
-            np.abs(self.scaler_inverse(y_pred) - self.scaler_inverse(y_true)).mean()
-        )
+        *_, y_pred_real, y_true_real = self.step(batch)
+        self.log("Val MAE (%)", np.abs(y_pred_real - y_true_real).mean(), prog_bar=True)
 
     def predict_step(self, batch, batch_idx, **kwargs):
-        x_uid, x_mid, y = batch
-        return self.scaler_inverse(self(x_uid, x_mid)), self.scaler_inverse(y)
+        x_uid, x_mid, *_, y_pred_real, y_true_real = self.step(batch)
+
+        x_uid_real = self.uid_inverse(x_uid)
+        x_mid_real = self.mid_inverse(x_mid)
+
+        return x_uid_real, x_mid_real, y_pred_real, y_true_real
+
+    def step(self, batch):
+        x_uid, x_mid, y_true = batch
+        y_pred = self(x_uid, x_mid)
+        y_pred_real = self.scaler_inverse(y_pred)
+        y_true_real = self.scaler_inverse(y_true)
+
+        return x_uid, x_mid, y_pred, y_true, y_pred_real, y_true_real
 
     def configure_optimizers(self):
         optim = torch.optim.Adam(self.parameters(), lr=self.lr, weight_decay=0.001)
