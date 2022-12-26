0ee2deed1c2599ae65dc8eb384581be36969c298
Eve-ning <johnchangqi@hotmail.com>
2022-12-26 09:51:29 +0800

    Collapse repeated modules into blocks

--- opal/score/collaborative_filtering/neu_mf_net.py
+++ opal/score/collaborative_filtering/neu_mf_net.py
@@ -1,6 +1,18 @@
 import torch
 import torch.nn as nn
-from torch.nn.functional import tanh
+
+
+class NeuMFBlock(nn.Module):
+    def __init__(self, in_chn, out_chn, dropout: float = 0.1):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(in_chn, out_chn),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+        )
+
+    def forward(self, x):
+        return self.net(x)
 
 
 class NeuMFNet(nn.Module):
@@ -10,32 +22,18 @@ class NeuMFNet(nn.Module):
         self.u_mf_emb = nn.Embedding(uid_no, mf_emb_dim)
         self.m_mf_emb = nn.Embedding(mid_no, mf_emb_dim)
         self.mf_net = nn.Sequential(
-            nn.Linear(mlp_emb_dim, mlp_emb_dim),
-            nn.ReLU(),
-            nn.Dropout(0.1),
-            nn.Linear(mlp_emb_dim, mlp_emb_dim),
-            nn.ReLU(),
-            nn.Dropout(0.1),
+            NeuMFBlock(mlp_emb_dim, mlp_emb_dim),
+            NeuMFBlock(mlp_emb_dim, mlp_emb_dim),
         )
         self.u_mlp_emb = nn.Embedding(uid_no, mlp_emb_dim)
         self.m_mlp_emb = nn.Embedding(mid_no, mlp_emb_dim)
         self.mlp_net = nn.Sequential(
-            nn.Linear(mlp_emb_dim * 2, 512),
-            nn.ReLU(),
-            nn.Dropout(0.1),
-            nn.Linear(512, 256),
-            nn.ReLU(),
-            nn.Dropout(0.1),
-            nn.Linear(256, 128),
-            nn.ReLU(),
-            nn.Dropout(0.1),
-            nn.Linear(128, 64),
-            nn.ReLU(),
-            nn.Dropout(0.1),
-            nn.Linear(64, 32),
-            nn.ReLU(),
-            nn.Dropout(0.1),
-            nn.Linear(32, mlp_chn_out),
+            NeuMFBlock(mlp_emb_dim * 2, 512),
+            NeuMFBlock(512, 256),
+            NeuMFBlock(256, 128),
+            NeuMFBlock(128, 64),
+            NeuMFBlock(64, 32),
+            NeuMFBlock(32, mlp_chn_out),
         )
         self.neu_mf_net = nn.Sequential(
             nn.Linear(mlp_chn_out + mf_emb_dim, 1),
@@ -51,6 +49,6 @@ class NeuMFNet(nn.Module):
         m_mlp_emb = self.m_mlp_emb(mid)
         mlp_out = self.mlp_net(torch.concat([u_mlp_emb, m_mlp_emb], dim=-1))
 
-        pred = self.neu_mf_net(torch.concat([mf_out, mlp_out], dim=-1)) * 2
+        pred = self.neu_mf_net(torch.concat([mf_out, mlp_out], dim=-1))
 
-        return torch.tanh(pred)[:, :, 0] * 5
+        return torch.tanh(pred)[:, :, 0]
