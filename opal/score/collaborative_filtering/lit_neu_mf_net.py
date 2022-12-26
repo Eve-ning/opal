01111ecb93780b7f8e0856227b17e155907072a2
Eve-ning <johnchangqi@hotmail.com>
2022-12-26 09:51:42 +0800

    Add save_hyperparams

--- opal/score/collaborative_filtering/lit_neu_mf_net.py
+++ opal/score/collaborative_filtering/lit_neu_mf_net.py
@@ -2,6 +2,7 @@ import numpy as np
 import pytorch_lightning as pl
 import torch
 from sklearn.base import TransformerMixin
+from sklearn.preprocessing import QuantileTransformer
 from torch.nn import MSELoss
 from torch.optim.lr_scheduler import StepLR
 
@@ -9,13 +10,14 @@ from opal.score.collaborative_filtering.neu_mf_net import NeuMFNet
 
 
 class LitNeuMFNet(pl.LightningModule):
-    def __init__(self, uid_no, mid_no, mf_emb_dim, mlp_emb_dim, mlp_chn_out, scaler: TransformerMixin,
+    def __init__(self, uid_no, mid_no, mf_emb_dim, mlp_emb_dim, mlp_chn_out, scaler: QuantileTransformer,
                  lr: float = 0.005):
         super().__init__()
         self.model = NeuMFNet(uid_no, mid_no, mf_emb_dim, mlp_emb_dim, mlp_chn_out)
         self.loss = MSELoss()
         self.scaler = scaler
         self.lr = lr
+        self.save_hyperparameters(ignore=['scaler'])
 
     def forward(self, uid, mid):
         return self.model(uid, mid)
@@ -26,13 +28,10 @@ class LitNeuMFNet(pl.LightningModule):
     def training_step(self, batch, batch_idx):
         x_uid, x_mid, y = batch
         y_hat = self(x_uid, x_mid)
-        # y_adj = adj_inv_sigmoid(y)
 
         if batch_idx % 32 == 0:
             self.logger.experiment.add_histogram("pred", y_hat)
             self.logger.experiment.add_histogram("true", y)
-            # self.logger.experiment.add_histogram("pred_learn", y_hat)
-            # self.logger.experiment.add_histogram("true_learn", y_adj)
 
         loss = self.loss(y_hat, y)
         self.log(
@@ -47,7 +46,6 @@ class LitNeuMFNet(pl.LightningModule):
     def validation_step(self, batch, batch_idx):
         x_uid, x_mid, y = batch
         y_hat = self(x_uid, x_mid)
-        # y_adj = adj_inv_sigmoid(y)
         self.log(
             "val_mae",
             np.abs(
