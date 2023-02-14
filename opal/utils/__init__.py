3c2b12dc82ccade4abb3cb0eda3edb342adf07e3
Eve-ning <dev_evening@hotmail.com>
2023-02-14 15:53:52 +0800

    Remove unused utils

--- opal/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from .ecdf import ecdf
-from .load_map import load_replay, load_replays
-
-__all__ = ['load_replay', 'load_replays']
--- opal/utils/ecdf.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-
-def ecdf(x: pd.Series):
-    counts = x.value_counts()
-    x = counts.sort_index().cumsum() / len(counts)
-    x.index = x.index.get_level_values(0)
-    x /= x.max()
-    return x
