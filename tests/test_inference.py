84431c3f6e0cd14407361a308c6cd56c950d4653
Evening <johnchangqi@hotmail.com>
2023-05-03 15:19:51 +0800

    Update test variable naming scheme

--- tests/test_inference.py
+++ tests/test_inference.py
@@ -4,19 +4,20 @@ from opal import OpalNet
 
 
 @pytest.fixture(scope="session")
-def net():
+def opal():
     return OpalNet.load(is_eval=True)
 
 
-def test_inference_single(net):
-    assert net.predict("2193881/2020", "767046/0") > 0.9
+def test_inference_single(opal):
+    assert opal.predict("2193881/2020", "767046/0") > 0.9
 
 
-def test_inference_list(net):
-    preds = net.predict(["2193881/2020", "2193881/2017"], ["767046/0", "767046/1"])
+def test_inference_list(opal):
+    preds = opal.predict(["2193881/2020", "2193881/2017"], ["767046/0", "767046/1"])
     assert all([p > 0.9 for p in preds])
 
 
-def test_missing_inference(net):
+def test_missing_inference(opal):
     with pytest.raises(ValueError):
-        net.predict("2193881/2018", "767046/0")
+        opal.predict("2193881/2018", "767046/0")
+
