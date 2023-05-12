cf0074968321c1a7f61c7d7f07442cfeaadcf338
Evening <johnchangqi@hotmail.com>
2023-05-12 23:36:17 +0800

    Fine-tune model script

--- opal/train.py
+++ opal/train.py
@@ -1,16 +1,14 @@
 import pytorch_lightning as pl
 import torch
-from pytorch_lightning.callbacks import LearningRateMonitor, EarlyStopping, ModelCheckpoint
+from pytorch_lightning.callbacks import LearningRateMonitor, EarlyStopping, ModelCheckpoint, StochasticWeightAveraging
 
-from opal.module import OpalNet
 from opal.datamodule import ScoreDataModule
+from opal.module import OpalNet
 
 
-def train(yyyy_mm: str):
+def train(version: str):
     dm = ScoreDataModule(
-        ds_yyyy_mm=yyyy_mm,
-        batch_size=2 ** 10,
-        ds_set="10000",
+        batch_size=2 ** 7,
         accuracy_bounds=(0.85, 1.0)
     )
 
@@ -18,24 +16,28 @@ def train(yyyy_mm: str):
     net = OpalNet(
         uid_le=dm.uid_le,
         mid_le=dm.mid_le,
-        qt=dm.qt_accuracy,
+        transformer=dm.transformer,
         emb_dim=8,
-        mf_repeats=2,
+        mf_repeats=3,
         mlp_range=[128, 64, 32, 8],
         lr=1e-3,
+        lr_gamma=0.75,
     )
 
     trainer = pl.Trainer(
         deterministic=True,
         max_epochs=epochs,
         accelerator='gpu',
-        default_root_dir="V1_2022_11",
+        default_root_dir=version,
+
         callbacks=[
+            StochasticWeightAveraging(swa_lrs=1e-2),
             EarlyStopping(
                 monitor="val_loss",
                 patience=3,
                 verbose=True,
                 mode='min',
+                min_delta=0.0001,
                 divergence_threshold=1
             ),
             LearningRateMonitor(),
@@ -48,4 +50,4 @@ def train(yyyy_mm: str):
 
 if __name__ == '__main__':
     torch.set_float32_matmul_precision('high')
-    train("2023_04")
+    train("V3_2023_04")
