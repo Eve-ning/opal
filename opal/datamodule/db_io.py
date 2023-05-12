5661910af09bd2e8d7d4664c344c53e9fa1f070c
Evening <johnchangqi@hotmail.com>
2023-05-12 14:18:45 +0800

    Update DB IO to use pre-initialized temp tables per query

--- opal/datamodule/db_io.py
+++ opal/datamodule/db_io.py
@@ -10,38 +10,29 @@ mysql_pw = os.environ['mysql_pw']
 class DB:
     mysql_engine = create_engine(f'mysql+mysqlconnector://osu:{mysql_pw}@localhost:3306/osu')
 
-    def get_df_score(
+    def __init__(
             self,
             min_active_map: int = 50,
             min_active_user: int = 50,
             accuracy_bounds: tuple[float, float] = (0.85, 1.0),
             keys: tuple[int] = (4, 7),
     ):
-        """ Get Data Frame for Relevant Scores
+        """ Initializes a database instance to interact with MySQL
 
         Args:
             min_active_map: Minimum number of scores for a beatmap to be included
             min_active_user: Minimum number of scores for a user to be included
             keys: Tuple of keys to include
             accuracy_bounds: The lower and upper bound of accuracies to include
-
-        Returns:
-            A DataFrame of:
-            - score_id Index: Unique Score IDs, as per osu!'s db
-            - uid: User ID
-            - year: Year of Play
-            - mid: Beatmap (Map) ID
-            - speed: Speed of map {-1, 0, 1} to indicate {HT, NT, DT}
-            - accuracy: Accuracy of play
         """
-        pbar = tqdm(total=6, desc="Querying from database...", unit="Step")
-        con = self.mysql_engine.connect()
-        con.execute(
+        pbar = tqdm(total=5, desc="Initializating Temporary Tables...")
+        self.con = self.mysql_engine.connect()
+        self.con.execute(
             text("DROP TEMPORARY TABLE IF EXISTS beatmaps, beatmap_scores, scores, active_uid, active_mid")
         )
 
         pbar.update()
-        con.execute(text(f"""
+        self.con.execute(text(f"""
             CREATE TEMPORARY TABLE
                 beatmaps (SELECT beatmap_id mid
                           FROM osu_beatmaps b
@@ -50,7 +41,7 @@ class DB:
         """))
 
         pbar.update()
-        con.execute(text(f"""
+        self.con.execute(text(f"""
             CREATE TEMPORARY TABLE
                 scores (SELECT s.score_id     sid,
                                s.beatmap_id   mid,
@@ -63,7 +54,7 @@ class DB:
         """))
 
         pbar.update()
-        con.execute(text("""
+        self.con.execute(text("""
             CREATE TEMPORARY TABLE beatmap_scores (
                 sid INT NOT NULL,
                 mid INT NOT NULL,
@@ -84,7 +75,7 @@ class DB:
         """))
 
         pbar.update()
-        con.execute(text(f"""
+        self.con.execute(text(f"""
             CREATE TEMPORARY TABLE active_mid (
                 mid INT,
                 speed INT,
@@ -96,7 +87,7 @@ class DB:
         """))
 
         pbar.update()
-        con.execute(text(f"""
+        self.con.execute(text(f"""
             CREATE TEMPORARY TABLE active_uid (
                 uid INT,
                 year INT,
@@ -107,14 +98,18 @@ class DB:
               HAVING COUNT(0) > {min_active_user};
         """))
 
-        pbar.update()
-        df = pd.read_sql(
+    def get_df_score(self):
+        """ Get Data Frame for Relevant Scores
+        Returns:
+            A DataFrame of:
+            - score_id Index: Unique Score IDs, as per osu!'s db
+            - uid: User ID
+            - year: Year of Play
+            - mid: Beatmap (Map) ID
+            - speed: Speed of map {-1, 0, 1} to indicate {HT, NT, DT}
+            - accuracy: Accuracy of play
+        """
+        return pd.read_sql(
             "SELECT * FROM beatmap_scores NATURAL JOIN active_mid NATURAL JOIN active_uid;",
-            con=con
+            con=self.con
         ).set_index('sid')
-
-        # Clean up temp tables
-        con.execute(
-            text("DROP TEMPORARY TABLE IF EXISTS beatmaps, beatmap_scores, scores, active_uid, active_mid")
-        )
-        return df
