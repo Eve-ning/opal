dd5ec111d1183c9e9b2c98fabf816885cc4d6348
Evening <johnchangqi@hotmail.com>
2023-08-03 15:12:55 +0800

    Remove unused func

--- opal/conf/conf.py
+++ opal/conf/conf.py
@@ -7,10 +7,3 @@ PKG_DIR = PROJ_DIR / "opal"
 
 MODEL_DIR = PKG_DIR / "models/"
 LATEST_MODEL_CKPT = MODEL_DIR / "V3_2023_05/model.ckpt"
-
-
-def get_current_files_dir() -> Path:
-    """ Gets the current local docker mounted osu.files directory """
-    files_dir = PROJ_DIR / "opal.files" / f"{datetime.now().strftime('%Y_%m')}_01_osu_files/"
-    assert files_dir.is_dir(), f"Failed to get files dir, {files_dir.as_posix()} is not a directory."
-    return files_dir
