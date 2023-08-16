fa723d06f65cabc0519d49f59add0cb2a87def27
Evening <johnchangqi@hotmail.com>
2023-08-16 23:26:54 +0800

    Update naming

--- scripts/preprocess/preprocess.sh
+++ scripts/preprocess/preprocess.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 
-create_opal_tables() {
+compute_opal_tables() {
   if ! docker ps | grep -q osu.mysql; then
-    echo -e "\e[osu.mysql is not running, please run it first\e[0m"
+    echo -e "\e[osu.mysql is not running.\e[0m"
     exit 1
   fi
 
   # We'll check if opal_active_scores, the table to train opal, is present
   # If not, then we run the .sql to generate it, which takes a few minutes.
-  if docker exec opal.mysql mysql \
+  if docker exec osu.mysql mysql \
     -u root --password=p@ssw0rd1 -D osu \
     -e 'SELECT * FROM opal_active_scores LIMIT 1;' >>/dev/null 2>&1; then
     echo -e "\e[33mopal_active_scores is present, skip creating opal tables\e[0m"
   else
     echo -e "\e[33mopal_active_scores is absent, creating opal tables\e[0m"
     echo -e "\e[32mThis will take around 10 minutes\e[0m"
-    docker exec -i opal.mysql mysql -u root --password=p@ssw0rd1 -D osu <./create_opal_tables.sql
+    docker exec -i osu.mysql mysql -u root --password=p@ssw0rd1 -D osu <./create_opal_tables.sql
     echo -e "\e[33mopal tables created\e[0m"
   fi
-
-  exit 0
 }
 
 compute_visual_complexity() {
@@ -36,6 +34,8 @@ cd_to_script() {
 docker_compose_up() {
   # We have to run compose WITHIN the submod, so it can detect dependent sh files.
   cd osu-data-docker/ || exit 1
+  echo "Moving to osu-data-docker"
+  echo "Docker Compose Up"
   docker compose \
     --profile files \
     -f docker-compose.yml \
@@ -43,10 +43,13 @@ docker_compose_up() {
     --env-file ../osu-data-docker.env \
     up --wait --build
   cd .. || exit 1
+  echo "Moving to out of osu-data-docker"
 }
 
 docker_compose_down() {
   cd osu-data-docker/ || exit 1
+  echo "Moving to osu-data-docker"
+  echo "Docker Compose Stop"
   docker compose \
     --profile files \
     -f docker-compose.yml \
@@ -54,10 +57,11 @@ docker_compose_down() {
     --env-file ../osu-data-docker.env \
     stop
   cd .. || exit 1
+  echo "Moving to out of osu-data-docker"
 }
 
 cd_to_script
 docker_compose_up
 create_opal_tables
-compute_visual_complexity
+compute_opal_svness
 docker_compose_down
