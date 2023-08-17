290c0a8ba5b59f81695cb6c4730e3c02918a43cb
Evening <johnchangqi@hotmail.com>
2023-08-17 19:38:49 +0800

    Add dataset path

--- opal/conf/conf.py
+++ opal/conf/conf.py
@@ -4,6 +4,7 @@ from pathlib import Path
 # PROJ_DIR is opal/, PKG_DIR is opal/opal/
 PROJ_DIR = Path(__file__).parents[2]
 PKG_DIR = PROJ_DIR / "opal"
+DATASET_DIR = PROJ_DIR / "datasets"
 
 MODEL_DIR = PKG_DIR / "models/"
 LATEST_MODEL_CKPT = MODEL_DIR / "V3_2023_05/model.ckpt"
