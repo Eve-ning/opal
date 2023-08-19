956c2f65193a5392ad90cf457f07b4f1e5b7a457
Evening <johnchangqi@hotmail.com>
2023-08-19 22:53:44 +0800

    Make conf path invariant for windows

--- src/opal/conf.py
+++ src/opal/conf.py
@@ -2,10 +2,11 @@ from pathlib import Path
 import platform
 
 if platform.system() == 'Windows':
-    ROOT_DIR = Path("../")
+    ROOT_DIR = Path(__file__).parents[1]
 else:
     ROOT_DIR = Path("/var/lib/opal/")
 DATASET_DIR = ROOT_DIR / "datasets"
 MODEL_DIR = ROOT_DIR / "models"
+EXPERIMENT_DIR = ROOT_DIR / "lightning_logs"
 
 LATEST_MODEL_CKPT = MODEL_DIR / "V3_2023_05/model.ckpt"
