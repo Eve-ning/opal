21c307817bf6b0fdfc5587c50fc9cd3575f51254
Evening <johnchangqi@hotmail.com>
2023-08-21 23:32:08 +0800

    Update latest model path

--- src/opal/conf.py
+++ src/opal/conf.py
@@ -1,5 +1,5 @@
-from pathlib import Path
 import platform
+from pathlib import Path
 
 if platform.system() == 'Windows':
     ROOT_DIR = Path(__file__).parents[1]
@@ -9,4 +9,8 @@ DATASET_DIR = ROOT_DIR / "datasets"
 MODEL_DIR = ROOT_DIR / "models"
 EXPERIMENT_DIR = ROOT_DIR / "lightning_logs"
 
-LATEST_MODEL_CKPT = MODEL_DIR / "V3_2023_05/model.ckpt"
+LATEST_MODEL_CKPT = (
+        MODEL_DIR /
+        "V4/2023_08_01_performance_mania_top_10000_20230819163602.csv/"
+        "lightning_logs/version_2/evaluation/model.ckpt"
+)
