a6f179ee1804f863aa81d6e601663a8d00575876
Evening <johnchangqi@hotmail.com>
2023-08-23 12:05:10 +0800

    Update conf mapping

--- src/opal/conf.py
+++ src/opal/conf.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 ROOT_DIR = Path(__file__).parents[1]
 DATASET_DIR = ROOT_DIR / "datasets"
-MODEL_DIR = ROOT_DIR / "models"
+MODEL_DIR = ROOT_DIR / "opal/models"
 EXPERIMENT_DIR = ROOT_DIR / "lightning_logs"
 
 LATEST_MODEL_CKPT = (
