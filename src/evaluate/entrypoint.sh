866caa0c95217f98a8930c4ebfb74ef7795a6e77
Evening <johnchangqi@hotmail.com>
2023-08-24 22:38:55 +0800

    Update evaluate to pipeline

---
+++ src/evaluate/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Ensure that one argument is passed
+if [ "$#" -ne 1 ]; then
+  echo "$0 missing PIPELINE_RUN_CACHE argument"
+  exit 1
+fi
+
+PIPELINE_RUN_CACHE="$1"
+
+# Sources the run cache
+. $PIPELINE_RUN_CACHE
+
+python -m opal.evaluate --model_path "$MODEL_PATH" --dataset_name "$DATASET_NAME" || exit 1
