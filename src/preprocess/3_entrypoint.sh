2da1d6eb08704b6bbf80bbf2d779ecc83ab9f169
Evening <johnchangqi@hotmail.com>
2023-08-25 20:21:24 +0800

    Implement sub for sql args

--- src/preprocess/3_entrypoint.sh
+++ src/preprocess/3_entrypoint.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-mysql -h osu.mysql -P 3307 -u root -pp@ssw0rd1 -D osu <./3_preprocess.sql
+
+SQL_SCRIPT=$(cat ./3_preprocess.sql)
+
+: "${MAX_SVNESS:=0.05}"
+
+SQL_SCRIPT=${SQL_SCRIPT//__MAX_SVNESS__/"$MAX_SVNESS"}
+
+echo "$SQL_SCRIPT" | mysql -h osu.mysql -P 3307 -u root -pp@ssw0rd1 -D osu
