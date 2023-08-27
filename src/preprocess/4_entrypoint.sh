08a1a9fb015600f7728e5238a067a6d74760ac2a
Evening <johnchangqi@hotmail.com>
2023-08-27 22:56:55 +0800

    Make 4 use env

--- src/preprocess/4_entrypoint.sh
+++ src/preprocess/4_entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-EXPORT_NAME="$1"
+
+# Ensure all variables are set
+: "${DATASET_NAME:?DATASET_NAME not set}"
 
 mysql -h osu.mysql -P 3307 -u root -pp@ssw0rd1 -D osu < \
 ./4_export.sql | \
-sed 's/\t/,/g' >../datasets/"${EXPORT_NAME}"
+sed 's/\t/,/g' >../datasets/"${DATASET_NAME}"
+
+exit 0
