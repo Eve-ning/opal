6da4bb5a274252eabc43ee02c69dbec96686a068
Evening <johnchangqi@hotmail.com>
2023-08-27 22:57:02 +0800

    Make 1 wait for mysql

--- src/preprocess/1_entrypoint.sh
+++ src/preprocess/1_entrypoint.sh
@@ -16,4 +16,13 @@ SQL_SCRIPT=${SQL_SCRIPT//__ACC_MAX__/"$ACC_MAX"}
 SQL_SCRIPT=${SQL_SCRIPT//__MIN_SCORES_PER_MID__/"$MIN_SCORES_PER_MID"}
 SQL_SCRIPT=${SQL_SCRIPT//__MIN_SCORES_PER_UID__/"$MIN_SCORES_PER_UID"}
 
+# Ping mysql until it's ready with 3 retries of 5s interval
+for _ in {1..3}; do
+  if mysqladmin ping -h osu.mysql -P 3307 -u root -pp@ssw0rd1; then
+    break
+  fi
+  echo "Waiting for osu.mysql to be ready..."
+  sleep 5
+done
+
 echo "$SQL_SCRIPT" | mysql -h osu.mysql -P 3307 -u root -pp@ssw0rd1 -D osu
