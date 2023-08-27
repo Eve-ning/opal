d6dc5663b86e418f995449dfa7a7121e2767c9e3
Evening <johnchangqi@hotmail.com>
2023-08-27 20:04:23 +0800

    Avoid loading from cache for more explicit arg

--- src/evaluate/entrypoint.sh
+++ src/evaluate/entrypoint.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
-# Ensure that one argument is passed
-if [ "$#" -ne 1 ]; then
-  echo "$0 missing PIPELINE_RUN_CACHE argument"
-  exit 1
-fi
 
-PIPELINE_RUN_CACHE="$1"
-
-# Sources the run cache
-. $PIPELINE_RUN_CACHE
+# Ensure all variables are set
+: "${MODEL_PATH:?MODEL_PATH not set}"
+: "${DATASET_NAME:?DATASET_NAME not set}"
 
 python -m opal.evaluate \
---model_path "$MODEL_PATH" \
---dataset_name "$DATASET_NAME" \
-|| exit 1
+  --model_path "$MODEL_PATH" \
+  --dataset_name "$DATASET_NAME" ||
+  exit 1
