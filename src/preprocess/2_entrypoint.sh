8a7611a343762c6bd5fecc3e542fc070fb79a281
Evening <johnchangqi@hotmail.com>
2023-08-27 20:04:29 +0800

    Avoid loading from cache for more explicit arg

--- src/preprocess/2_entrypoint.sh
+++ src/preprocess/2_entrypoint.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
-# Ensure that one argument is passed
-if [ "$#" -ne 1 ]; then
-  echo "$0 missing PIPELINE_RUN_CACHE argument"
-  exit 1
-fi
-
-PIPELINE_RUN_CACHE="$1"
-
-# Sources the run cache
-. $PIPELINE_RUN_CACHE
-
 
 python -m opal.svness \
---files_path "${FILES_DIR}" \
---db_name "${DB_NAME}" \
---db_username "${DB_USERNAME}" \
---db_password "${DB_PASSWORD}" \
---db_host "${DB_HOST}" \
---db_port "${DB_PORT}" \
-|| exit 1
+  --files_path "${FILES_DIR}" \
+  --db_name "${DB_NAME}" \
+  --db_username "${DB_USERNAME}" \
+  --db_password "${DB_PASSWORD}" \
+  --db_host "${DB_HOST}" \
+  --db_port "${DB_PORT}" ||
+  exit 1
