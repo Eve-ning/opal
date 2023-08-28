7710e2e6b112c94a0cbd8d4e987cdc353437a6a5
Evening <johnchangqi@hotmail.com>
2023-08-28 22:00:33 +0800

    Revert hacky

--- src/pipeline.sh
+++ src/pipeline.sh
@@ -18,49 +18,33 @@ mkdir -p datasets
   echo "Pipeline run cache ${PIPELINE_RUN_CACHE} already exists" && exit 1
 
 # Set default values for variables
-export DB_URL=https://github.com/Eve-ning/opal/raw/fix-pipeline2/rsc/sample.tar.bz2
-export FILES_URL=https://github.com/Eve-ning/opal/raw/fix-pipeline2/rsc/sample_files.tar.bz2
+DB_URL=https://github.com/Eve-ning/opal/raw/fix-pipeline2/rsc/sample.tar.bz2
+FILES_URL=https://github.com/Eve-ning/opal/raw/fix-pipeline2/rsc/sample_files.tar.bz2
 cat <<EOF >>"$PIPELINE_RUN_CACHE"
-PIPELINE_RUN_CACHE="$PIPELINE_RUN_CACHE"
-DB_URL="$DB_URL"
-FILES_URL="$FILES_URL"
-FILES_DIR=/"var/lib/osu/osu.files/$(basename "$FILES_URL" .tar.bz2)/"
-MODEL_NAME="2023.8.4b"
-DATASET_NAME="$(basename "$DB_URL" .tar.bz2)_$(date +"%Y%m%d%H%M%S").csv"
-DB_NAME="osu"
-DB_USERNAME="root"
-DB_PASSWORD="p@ssw0rd1"
-DB_HOST="osu.mysql"
-DB_PORT="3307"
-SR_MIN="2"
-SR_MAX="15"
-ACC_MIN="0.85"
-ACC_MAX="1.0"
-MIN_SCORES_PER_MID="0"
-MIN_SCORES_PER_UID="0"
-MAX_SVNESS="0.05"
+PIPELINE_RUN_CACHE=$PIPELINE_RUN_CACHE
+DB_URL=$DB_URL
+FILES_URL=$FILES_URL
+FILES_DIR=/var/lib/osu/osu.files/$(basename "$FILES_URL" .tar.bz2)/
+MODEL_NAME=2023.8.4b
+DATASET_NAME=$(basename "$DB_URL" .tar.bz2)_$(date +"%Y%m%d%H%M%S").csv
+DB_NAME=osu
+DB_USERNAME=root
+DB_PASSWORD=p@ssw0rd1
+DB_HOST=osu.mysql
+DB_PORT=3307
+SR_MIN=2
+SR_MAX=15
+ACC_MIN=0.85
+ACC_MAX=1.0
+MIN_SCORES_PER_MID=0
+MIN_SCORES_PER_UID=0
+MAX_SVNESS=0.05
 EOF
 
 # Source and Export variables
+set -a
 source "$PIPELINE_RUN_CACHE"
-export PIPELINE_RUN_CACHE
-export DB_URL
-export FILES_URL
-export FILES_DIR
-export MODEL_NAME
-export DATASET_NAME
-export DB_NAME
-export DB_USERNAME
-export DB_PASSWORD
-export DB_HOST
-export DB_PORT
-export SR_MIN
-export SR_MAX
-export ACC_MIN
-export ACC_MAX
-export MIN_SCORES_PER_MID
-export MIN_SCORES_PER_UID
-export MAX_SVNESS
+set +a
 
 echo "Preprocessing"
 docker compose \
