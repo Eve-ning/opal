17bd4832dcb3e5eba694b5d4efa0ff8d2d7e3c3c
Evening <johnchangqi@hotmail.com>
2023-09-05 22:13:20 +0800

    Update doc to env file

--- src/train/entrypoint.sh
+++ src/train/entrypoint.sh
@@ -7,7 +7,7 @@
 
 # TODO: Figure out a way for opal.train to set env var in shell instead of within python
 #       An idea is to specify the model version number `version_XXXX` then we can grep for it.
-# This internally sets the MODEL_PATH env var in the pipeline run cache
+# This internally sets the MODEL_PATH env var in the env file
 python -m opal.train \
   --dataset_name "$DATASET_NAME" \
   --model_name "$MODEL_NAME" || exit 1
