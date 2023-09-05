fa4ece7c2c93d6760556d93abcd7aa2ec2338203
Evening <johnchangqi@hotmail.com>
2023-09-05 21:40:39 +0800

    Remove redundant script for caching env

--- src/pipeline.sh
+++ src/pipeline.sh
@@ -16,39 +16,14 @@
 # Change directory to current script directory
 cd "$(dirname "$(realpath "$0")")" || exit 1
 
-. ./utils
+. ./utils.sh
 
 # Preprocesses the Dataset.
 # Sets the DATASET_NAME variable in the pipeline run cache.
 preprocess() {
-  envdotsub preprocess/docker-compose.yml
-  sed -i 's|osu-data-docker/docker-compose.yml|osu-data-docker/.docker-compose.yml|g' \
-    preprocess/.docker-compose.yml || exit 1
-  envdotsub preprocess/osu-data-docker/docker-compose.yml
-
-  # Without -d, this script will hang until the docker compose process is killed
-  # To also include compose stop, we'll peek at the dataset file and wait for it to be created
-  docker compose \
-    --profile files \
-    -f preprocess/.docker-compose.yml \
-    up --build -d >preprocess.log 2>&1 &
-
-  while [ ! -f "./datasets/$DATASET_NAME" ]; do
-    echo "Waiting for dataset to be created... (Showing most recent log)"
-    tail -n 3 preprocess.log
-    sleep 10
-  done
-
-  docker compose \
-    --profile files \
-    -f preprocess/.docker-compose.yml \
-    stop || exit 1
-
-  source "$PIPELINE_RUN_CACHE"
-  if [ -z "$DATASET_NAME" ]; then
-    echo "DATASET_NAME not returned by preprocess"
-    exit 1
-  fi
+  cd preprocess || exit 1
+  ./run.sh ../.env || exit 1
+  cd .. || exit 1
 }
 
 # Trains the Model.
@@ -84,30 +59,10 @@ publish() {
     up --build || exit 1
 }
 
-make_pipeline_cache() {
-  # Create unique pipeline run id
-  PIPELINE_RUN_CACHE=.pipeline_cache/${1:-$(date +%s)}.env
-  mkdir -p .pipeline_cache
-  mkdir -p datasets
-  if [ -f "$PIPELINE_RUN_CACHE" ]; then
-    echo "Pipeline run cache ${PIPELINE_RUN_CACHE} already exists"
-    exit 1
-  fi
-}
-
-load_env() {
-  # Set default values for variables
-  set -a
-  source .env
-  set +a
-}
-
-make_pipeline_cache "$1" || exit 1
-load_env || exit 1
 preprocess || exit 1
-train || exit 1
-set -a
-source "$PIPELINE_RUN_CACHE"
-set +a
-evaluate || exit 1
-publish || exit 1
+#train || exit 1
+#set -a
+#source "$PIPELINE_RUN_CACHE"
+#set +a
+#evaluate || exit 1
+#publish || exit 1
