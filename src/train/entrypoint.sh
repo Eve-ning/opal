bd60ea992bec38c3f3533a7ad7ca0b168b770969
Evening <johnchangqi@hotmail.com>
2023-09-05 21:57:41 +0800

    Update naming

--- src/train/entrypoint.sh
+++ src/train/entrypoint.sh
@@ -3,7 +3,7 @@
 # Ensure all variables are set
 : "${DATASET_NAME:?DATASET_NAME not set}"
 : "${MODEL_NAME:?MODEL_NAME not set}"
-: "${PIPELINE_RUN_CACHE:?PIPELINE_RUN_CACHE not set}"
+: "${ENV_FILE_PATH:?ENV_FILE_PATH not set}"
 
 # TODO: Figure out a way for opal.train to set env var in shell instead of within python
 #       An idea is to specify the model version number `version_XXXX` then we can grep for it.
