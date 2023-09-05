2fb564425131299c37651cf11974273a718fb720
Evening <johnchangqi@hotmail.com>
2023-09-05 20:52:04 +0800

    Replace run.sh with independent entry script

--- src/preprocess/run.sh
+++ src/preprocess/run.sh
@@ -1,94 +1,77 @@
-#!/usr/bin/env bash
+# Usage: ./run.sh [ENV_FILE_PATH]
+#
+# The ENV_FILE_PATH is the path to the .env file to source.
+# The .env file should contain the following variables:
+# - SR_MIN
+# - SR_MAX
+# - ACC_MIN
+# - ACC_MAX
+# - MIN_SCORES_PER_MID
+# - MIN_SCORES_PER_UID
+# - FILES_DIR
+# - DB_NAME
+# - DB_USERNAME
+# - DB_PASSWORD
+# - DB_HOST
+# - DB_PORT
+# - MAX_SVNESS
+# - DATASET_NAME
 
-compute_opal_tables() {
-  if ! docker ps | grep -q osu.mysql; then
-    echo -e "\e[osu.mysql is not running.\e[0m"
-    exit 1
-  fi
+create_dataset_name() {
+  SQL_HASH=$(find . -type f -name "*.sql" -exec cat {} \; | sort | md5sum | cut -c1-8)
+  DATASET_HASH=$(echo "$SR_MIN$SR_MAX$ACC_MIN$ACC_MAX$MIN_SCORES_PER_MID$MIN_SCORES_PER_UID$MAX_SVNESS" | md5sum | cut -c1-8)
+  DATASET_NAME="$(basename "$DB_URL" .tar.bz2)_${DATASET_HASH}_${SQL_HASH}.csv"
 
-  # We'll check if opal_active_scores, the table to train opal, is present
-  # If not, then we run the .sql to generate it, which takes a few minutes.
-  if ! docker exec osu.mysql mysql \
-    -u root --password=p@ssw0rd1 -D osu \
-    -e 'SELECT * FROM opal_active_scores LIMIT 1;' >>/dev/null 2>&1; then
-    echo -e "\e[33mopal_active_scores is absent, creating opal tables (~10mins)\e[0m"
+  # echo with no new line
+  echo -e "\e[34mSQL_HASH:\e[0m $SQL_HASH "
+  echo -e "\e[34mDATASET_HASH:\e[0m $DATASET_HASH "
+  echo -e "\e[34mDB_NAME:\e[0m $DB_NAME"
+  echo -e "\e[34mDATASET_NAME:\e[0m" "$DATASET_NAME"
 
-    echo -e "\e[33m(1/3) Computing 1st opal table set\e[0m"
-    docker exec -i osu.mysql mysql -u root --password=p@ssw0rd1 -D osu <./compute_opal_tables_1.sql
-
-    # We define a network between the osu.mysql and our svness calc
-    if [ -z "$(docker network ls -f name=mysql-net -q)" ]; then
-      echo -e "\e[33mmysql-net not found, creating mysql-net\e[0m"
-      docker network create mysql-net
-    fi
-
-    docker network connect mysql-net osu.mysql >>/dev/null 2>&1
-    echo -e "\e[33(2/3) Computing SV-ness\e[0m"
-    docker build -t compute_opal_svness -f ./compute_opal_svness.Dockerfile .
-    docker run --rm --network mysql-net --mount type=bind,source="/var/lib/osu/",target="/var/lib/osu/" \
-      compute_opal_svness
-
-    echo -e "\e[33m(3/3) Computing 2nd opal table set\e[0m"
-    docker exec -i osu.mysql mysql -u root --password=p@ssw0rd1 -D osu <./compute_opal_tables_2.sql
-
-    echo -e "\e[33mopal tables created\e[0m"
-  fi
+  export DATASET_NAME
+  env_add "$FILE_PATH" "DATASET_NAME" "$DATASET_NAME"
 }
 
-cd_to_script() {
-  # We allow this script to be run from any location, we just force it to cd here before executing the rest.
-  PROJ_DIR="$(dirname "$(realpath "$0")")"
-  cd "$PROJ_DIR" || exit 1
-}
+# Check if the .env file path is the first argument and source it
+if [ -f "$1" ]; then
+  FILE_PATH="$1"
+  set -a
+  source "$FILE_PATH"
+  set +a
+else
+  echo "Usage: ./run.sh [ENV_FILE_PATH]"
+  exit 1
+fi
 
-docker_compose_up() {
-  # We have to run compose WITHIN the submod, so it can detect dependent sh files.
-  cd osu-data-docker/ || exit 1
-  docker compose \
-    --profile files \
-    -f docker-compose.yml \
-    --env-file .env \
-    --env-file ../osu-data-docker.env \
-    up --wait --build
-  cd .. || exit 1
-}
+# Load utils for envdotsub and check_env_set
+. ../utils.sh
+create_dataset_name
+export FILES_DIR="/var/lib/osu/osu.files/$(basename "$FILES_URL" .tar.bz2)/"
 
-docker_compose_down() {
-  cd osu-data-docker/ || exit 1
-  docker compose \
-    --profile files \
-    -f docker-compose.yml \
-    --env-file .env \
-    --env-file ../osu-data-docker.env \
-    stop
-  cd .. || exit 1
-}
+# Check if the dataset already exists
+if [ -f "../datasets/$DATASET_NAME" ]; then
+  # brown echo
+  echo -e "\e[33mDataset already exists: $DATASET_NAME\e[0m"
+  exit 0
+fi
 
-# Exports the opal_active_scores table to a csv file.
-export_opal_active_scores() {
-  docker exec -i osu.mysql mysql -u root --password=p@ssw0rd1 -D osu \
-    <./export_opal_active_scores.sql |
-    sed 's/\t/,/g' \
-      >"$1"
-}
+# Check if all necessary env vars are set
+check_env_set docker-compose.yml || exit 1
+check_env_set osu-data-docker/docker-compose.yml || exit 1
 
-PIPELINE_RUN_CACHE="$1"
-echo "PIPELINE_RUN_CACHE=$PIPELINE_RUN_CACHE"
-source "$PIPELINE_RUN_CACHE"
+envdotsub docker-compose.yml || exit 1
+envdotsub osu-data-docker/docker-compose.yml || exit 1
 
-# Ensure that the required variables are set
-[ -z "$DB_URL" ] && echo "DB_URL not set" && exit 1
-[ -z "$FILES_URL" ] && echo "FILES_URL not set" && exit 1
+# Without -d, this script will hang until the docker compose process is killed
+# To also include compose stop, we'll peek at the dataset file and wait for it to be created
+docker compose --profile files -f .docker-compose.yml up --build -d >preprocess.log 2>&1 &
 
-export DB_URL FILES_URL
+while [ ! -f "../datasets/$DATASET_NAME" ]; do
+  echo "Waiting for dataset to be created... (Showing most recent log)"
+  tail -n 3 preprocess.log
+  sleep 10
+done
 
-DATASET_NAME=$(basename "$DB_URL" .tar.bz2)_$(date +"%Y%m%d%H%M%S").csv
-DATASET_PATH=../datasets/$DATASET_NAME
+docker compose --profile files -f .docker-compose.yml stop || exit 1
 
-echo DATASET_NAME="$DATASET_NAME" >> "$PIPELINE_RUN_CACHE"
-
-cd_to_script
-docker_compose_up
-compute_opal_tables
-export_opal_active_scores "$DATASET_PATH"
-docker_compose_down
+echo "Preprocessing complete. Dataset: $DATASET_NAME"
